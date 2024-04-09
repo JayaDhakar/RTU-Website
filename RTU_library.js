409c686209f051cfafe708bdc2d21081a1d757ab
JayaDhakar <157195265+JayaDhakar@users.noreply.github.com>
2024-04-09 15:41:25 +0530

    Add files via upload

---
+++ RTU_library.js
@@ -0,0 +1,26 @@
+// let tab2 = document.getElementById("tab2").style.display = "none";
+// let tab3 = document.getElementById("tab3").style.display = "none";
+// let tab4 = document.getElementById("tab4").style.display = "none";
+// let tab6 = document.getElementById("tab6").style.display = "none";
+// let tab1 = document.getElementById("tab1").style.display = "block";
+
+// function showTabs(id) {
+//     console.log(id)
+//     tab1.style.display="none"
+//     document.getElementById(id).style.display= "block";
+// }
+    // var x = document.getElementById("tab1");
+    //   var y = document.getElementById("tab2");
+    //   var z = document.getElementById("btn");
+
+    //   function tab1() {
+    //     x.style.left = "-400px";
+    //     y.style.left = "50px";
+    //     z.style.left = "110px";
+    //   }
+    //   function tab2() {
+    //     x.style.left = "50px";
+    //     y.style.left = "450px";
+    //     z.style.left = "0";
+    //   }
+   
